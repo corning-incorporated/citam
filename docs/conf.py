72177fa4f9043bc2a2125fe050d6c87fe30fe989
Mardochee <reveilm2@corning.com>
2020-10-16 18:30:03 -0400

    more tests

--- docs/conf.py
+++ docs/conf.py
@@ -2,30 +2,30 @@ import os
 import sys
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, os.path.dirname(os.path.abspath('..')))
-sys.path.insert(0, os.path.dirname(os.path.abspath('../citam')))
+sys.path.insert(0, os.path.dirname(os.path.abspath("..")))
+sys.path.insert(0, os.path.dirname(os.path.abspath("../citam")))
 
 # -- Project information -----------------------------------------------------
-project = 'CITAM'
-copyright = '2020, Corning Incorporated'
-author = 'Mardochee Reveil, Chris Soper, Amit Jha'
+project = "CITAM"
+copyright = "2020, Corning Incorporated"
+author = "Mardochee Reveil, Chris Soper, Amit Jha"
 
-release = '0.9.0'
-master_doc = 'index'
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+release = "0.9.0"
+master_doc = "index"
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 extensions = [
-    'autoapi.extension',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.doctest',
-    'sphinx.ext.inheritance_diagram',
+    "autoapi.extension",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "sphinx.ext.inheritance_diagram",
     # 'sphinx.ext.autosectionlabel',
-    'sphinx.ext.mathjax',
-    'numpydoc',
-    'sphinx.ext.viewcode',
-    'sphinxarg.ext'
+    "sphinx.ext.mathjax",
+    "numpydoc",
+    "sphinx.ext.viewcode",
+    "sphinxarg.ext"
     # 'matplotlib.sphinxext.only_directives',
     # 'matplotlib.sphinxext.plot_directive',
     # 'matplotlib.sphinxext.ipython_directive',
@@ -45,17 +45,16 @@ html_theme = "citam_theme"
 html_theme_path = ["."]
 
 # html_static_path = ['_static']
-autoclass_content = 'both'
+autoclass_content = "both"
 autosummary_generate = True
 autodoc_inherit_docstrings = True
 
 autodoc_default_options = {
-    'member-order': "bysource",
-    'undoc-members': True,
-    'private-members': True,
-    'show-inheritence': True,
-    'members': None,
+    "member-order": "bysource",
+    "undoc-members": True,
+    "private-members": True,
+    "show-inheritence": True,
+    "members": None,
 }
-autoapi_type = 'python'
-autoapi_dirs = ['../citam']
-
+autoapi_type = "python"
+autoapi_dirs = ["../citam"]
