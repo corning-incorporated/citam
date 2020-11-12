1f51e0478de713ddab722c9658939716a0214733
Mardochee <reveilm2@corning.com>
2020-11-12 15:42:39 -0500

    add test for door serialization

--- citam/tests/engine/test_door.py
+++ citam/tests/engine/test_door.py
@@ -13,9 +13,12 @@ def test_serialize(rect_space):
     path = parse_path("M 0,0 L 100,0")
     test_door = Door(path=path, space1=rect_space)
 
-    obj = json.dumps(rect_space, default=serializer.encoder_default)
+    obj = json.dumps(test_door, default=serializer.encoder_default)
     door_decoded = json.loads(obj, object_hook=serializer.decoder_hook)
 
-    print(door_decoded, type(door_decoded))
-
-    assert 1 == 2
+    assert test_door.path == door_decoded.path
+    assert test_door.emergency_only == door_decoded.emergency_only
+    assert test_door.space1.id == door_decoded.space1
+    assert test_door.space2 == door_decoded.space2
+    assert test_door.in_service == door_decoded.in_service
+    assert test_door.special_access == door_decoded.special_access
