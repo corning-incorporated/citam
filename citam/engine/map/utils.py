cd50e90aa6fb687a9382580650bb063e1653d84b
Mardochee <reveilm2@corning.com>
2020-12-10 13:27:14 -0500

    add docstrings and type hints to floorplan and geometry

--- citam/engine/map/utils.py
+++ citam/engine/map/utils.py
@@ -12,11 +12,13 @@
 # WITH THE SOFTWARE OR THE USE OF THE SOFTWARE.
 # ==============================================================================
 
-import citam.engine.map.geometry as gsu
+from typing import Tuple
+
+import citam.engine.map.geometry as g
 from citam.engine.map.point import Point
 from citam.engine.map.space import Space
 
-from svgpathtools import Line
+from svgpathtools import Line, Path
 
 
 def find_closest_parallel_wall(space_boundaries, ref_wall):
@@ -46,14 +48,14 @@ def find_closest_parallel_wall(space_boundaries, ref_wall):
             continue
         if wall.length() <= 1.0:
             continue
-        x_overlap, y_overlap = gsu.calculate_x_and_y_overlap(ref_wall, wall)
+        x_overlap, y_overlap = g.calculate_x_and_y_overlap(ref_wall, wall)
         if x_overlap == 0 and y_overlap == 0:
             continue
-        dot_prod = gsu.calculate_dot_product_between_walls(ref_wall, wall)
+        dot_prod = g.calculate_dot_product_between_walls(ref_wall, wall)
 
         if abs(dot_prod - 1.0) < 0.1:
             # This wall is parallel to ref wall
-            dist = gsu.calculate_distance_between_walls(ref_wall, wall)
+            dist = g.calculate_distance_between_walls(ref_wall, wall)
             if dist < min_dist:
                 best_wall = wall
                 min_dist = dist
@@ -139,7 +141,7 @@ def get_aisle_center_point_and_width(aisle):
     """
 
     # Perpendicular vector between the two walls
-    V_perp = gsu.calculate_perpendicular_vector(aisle[0], aisle[1])
+    V_perp = g.calculate_perpendicular_vector(aisle[0], aisle[1])
     # Half way between the two walls from the middle of the first wall
     mid_point = aisle[0].point(0.5)
     center_point = Point(
@@ -152,7 +154,7 @@ def get_aisle_center_point_and_width(aisle):
     return center_point, width
 
 
-def is_this_an_aisle(wall1: Line, wall2: Line, space: Space):
+def is_this_an_aisle(wall1: Line, wall2: Line, space: Space) -> bool:
     """Verify that 2 walls form an aisle
 
     Works by verifying that the center point of this aisle is part
@@ -213,3 +215,112 @@ def compute_bounding_box(walls):
                 ymax = y
 
     return xmin, ymin, xmax, ymax
+
+
+def generate_closed_path_for_aisle(aisle: Tuple[Line, Line]) -> Path:
+    """
+    Given an aisle (tuple of two walls), add new walls to create a closed
+    path for this aisle.
+
+    :param aisle: Aisle of interest.
+    :type aisle: Tuple[Line, Line]
+    :return: Closed path for this aisle
+    :rtype: Path
+    """
+
+    path = Path()
+    path += [aisle[0], aisle[1]]
+
+    wall1 = aisle[0]
+    wall2 = aisle[1]
+
+    segment1 = Line(start=wall1.start, end=wall2.start)
+    segment2 = Line(start=wall1.start, end=wall2.end)
+
+    if segment1.length() <= segment2.length():
+        path.append(segment1)
+    else:
+        path.append(segment2)
+
+    segment3 = Line(start=wall1.end, end=wall2.start)
+    segment4 = Line(start=wall1.end, end=wall2.end)
+
+    if segment3.length() <= segment4.length():
+        path.append(segment3)
+    else:
+        path.append(segment4)
+
+    return path
+
+
+def do_walls_overlap(
+    wall1: Line, wall2: Line, max_distance: float = 1.0
+) -> bool:
+    """
+    Verify if two walls overlap.
+
+    Uses the start and end points of each line and verifies if either one
+    of them falls on the other segment. If so, it checks if all 4 points
+    are collinear and returns True if the dot product and the
+    distance between the 2 walls are respectively ~1.0 and ~0.0.
+
+    :param wall1:  line object corresponding to wall 1
+    :type wall1: Line
+    :param wall2: line object corresponding to wall 2
+    :type wall2: Line
+    :param max_distance: the max distance beyond which the walls are not
+            considered overlapping, defaults to 1.0
+    :type max_distance: float, optional
+    :return: Whether the walls overlap or not
+    :rtype: bool
+    """
+
+    x_overlap, y_overlap = g.calculate_x_and_y_overlap(wall1, wall2)
+
+    p1 = Point(x=round(wall1.start.real), y=round(wall1.start.imag))
+    q1 = Point(x=round(wall1.end.real), y=round(wall1.end.imag))
+
+    p2 = Point(x=round(wall2.start.real), y=round(wall2.start.imag))
+    q2 = Point(x=round(wall2.end.real), y=round(wall2.end.imag))
+
+    on_segment_test = False
+
+    if g.is_one_segment_within_the_other(p1, q1, p2, q2):
+        on_segment_test = True
+
+    # only one point from line being on the other is necessary
+    elif x_overlap > 0 or y_overlap > 0:
+        if g.do_lines_intersect_at_endpoint(p1, q1, p2, q2):
+            on_segment_test = True
+
+    if on_segment_test:
+
+        # Check if all 4 point are collinear
+        o1 = g.determine_orientation(p1, q1, p2)
+        o2 = g.determine_orientation(p1, q1, q2)
+
+        if o1 == o2 and o1 == 0:
+            return True
+
+        # only do this for walls that are diagonal
+        tol = 1e-2
+        if (
+            abs(p1.x - q1.x) < tol
+            or abs(p1.y - q1.y) < tol
+            or abs(p2.x - q2.x) < tol
+            or abs(p2.y - q2.y) < tol
+        ):
+            # At least one of the walls is horizontal or vertical
+            pass
+        else:
+            (
+                dot_product,
+                distance,
+            ) = g.calculate_dot_product_and_distance_between_walls(
+                wall1, wall2
+            )
+            if dot_product is not None:
+                if abs(dot_product - 1.0) < 1e-3 and distance < max_distance:
+                    return True
+
+    return False
