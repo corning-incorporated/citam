9c990dcbcafbee9d5c463c08baabda375730d427
Modini <ModiniK@corning.com>
2021-01-05 08:53:41 -0500

    Hide simulations when Facility changed

--- citamjs/src/App.vue
+++ citamjs/src/App.vue
@@ -65,12 +65,12 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Inter;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   background-color: black;
 }
 #mainLayout {
